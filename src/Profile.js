3ce8b47acd61d566eabaf99fcf9b8eb1ca295fab
mohamadir <mohamdib@gmail.com>
2017-12-16 12:16:22 +0200

    update

--- src/Profile.js
+++ src/Profile.js
@@ -8,12 +8,12 @@ export default class Profile extends Component {
     }
     render() {
         return (
-            <div class="row" style={{display:"flex",float:"left",margin:"40px"}}>
-                <div class="profile-header-container">   
-    		<div class="profile-header-img">
-                <img class="img-circle" style={{width:"100px" ,height:"100px"}}src="https://user-images.githubusercontent.com/17565537/34069100-4bd2150e-e252-11e7-9a94-ea2e6a77d5ba.png" />
-                <div class="rank-label-container">
-                    <span class="label label-default rank-label">{this.props.member.first_name} {this.props.member.last_name}</span>
+            <div className="row" style={{display:"flex",float:"left",margin:"40px"}}>
+                <div className="profile-header-container">   
+    		<div className="profile-header-img">
+                <img className="img-circle" style={{width:"100px" ,height:"100px"}}src="https://user-images.githubusercontent.com/17565537/34069100-4bd2150e-e252-11e7-9a94-ea2e6a77d5ba.png" />
+                <div className="rank-label-container">
+                    <span className="label label-default rank-label">{this.props.member.first_name} {this.props.member.last_name}</span>
                 </div>
             </div>
         </div> 
